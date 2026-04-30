1a460175bbf2663dc3fb5c8748615852b3186ac1
AmaanNasib <ammannasib005@gmail.com>
2026-04-30 16:05:31 +0530

    footer changed

--- client/src/components/Layout/CopyrightFooter.jsx
+++ client/src/components/Layout/CopyrightFooter.jsx
@@ -4,7 +4,7 @@ const CopyrightFooter = () => {
   const date = new Date();
   return (
     <div style={{ fontSize: 14, textAlign: "center", padding: '8px 48px', display: 'flex', justifyContent: 'space-between' }}>
-      <div>© {date.getFullYear()} Designed By <strong><a rel="noreferrer" href="https://www.linkedin.com/in/shizanvasim/" target="_blank">Sheezan Shaikh</a></strong></div>
+      <div>© {date.getFullYear()} Managed By <strong><a rel="noreferrer" href="https://giganexustechnology.com/" target="_blank">Giga Nexus Technology LLP</a></strong></div>
       <div>© {date.getFullYear()} <strong><a rel="noreferrer" href="http://internationaljournalofresearch.co.in/" target="_blank">IJR</a></strong>. All rights reserved</div>
     </div>
   );
